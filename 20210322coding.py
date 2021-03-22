daa113b70c09bd5222c5f033b132f4656be5e3f5
Otto Chang <schang96@gatech.edu>
2021-03-22 14:35:29 -0700

    upload new file

---
+++ 20210322coding.py
@@ -0,0 +1,24 @@
+x = 5
+print(x)
+x = 500
+x = 5000
+y = 2
+y = 2000
+print(y)
+
+price = 10
+pen = 0.38
+rain = True
+name = 'Otto'
+print(price)
+print(pen)
+print(rain)
+print(name)
+
+print(price, pen, rain, name)
+
+name = input('please input your name:')
+print('Your name:',name)
+
+Height = input('your height:')
+Weight = input('your weight:')
